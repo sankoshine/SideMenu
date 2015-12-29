36015abab269198e530266207344157306411223
SanKo <sankoshine@qq.com>
2015-12-29 15:53:19 +0800

    a simple realization of sidemenu

--- app/src/main/java/com/example/fragment_test/MainActivity.java
+++ app/src/main/java/com/example/fragment_test/MainActivity.java
@@ -1,13 +1,53 @@
 package com.example.fragment_test;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        Fragment fragment = null;
+        FragmentManager fragmentManager = getFragmentManager();
+        if (id == R.id.nav_camera) {
+            fragment = new FaceFragment();
+        } else if (id == R.id.nav_gallery) {
+            fragment = new TitleFragment();
+        }
+        else {
+            fragment = new FaceFragment();
+        }
+        fragmentManager.beginTransaction()
+                .replace(R.id.id_content, fragment)
+                .commit();
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
